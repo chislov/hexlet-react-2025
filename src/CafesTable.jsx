28c70dbf14c7a29b758fa02676cdfd4a498210a3
Alexander <chislov@gmail.com>
2025-12-25 18:13:27 +0300

    #2 fetch and display data works

--- src/CafesTable.jsx
+++ src/CafesTable.jsx
@@ -1,5 +1,46 @@
-export default function CafeTable() {
-    return (
-        <div>working</div>
-    )
+import { useState, useEffect } from 'react';
+
+export default function CafeList() {
+  const [cafes, setCafes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCafes = async () => {
+      try {
+        const response = await fetch('http://localhost:8070/cafes');
+        
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        
+        const data = await response.json();
+        setCafes(data.cafes); 
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCafes();
+  }, []); 
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+    return <div className='cafesTable'>
+        <ul className="cardsList">
+            {cafes.map(cafe => {
+                return <li className="card">
+                    <img src={cafe.img.length > 0 ? cafe.img : "https://via.placeholder.com/150"} alt={cafe.code} />
+                    <h2>{cafe.name}</h2>
+                    <p>{cafe.decs}</p>
+                    <p>{cafe.address}</p>
+                    <p>{cafe.subwayCode}</p>
+                    <p>{cafe.workTime}</p>
+                </li>
+            })}
+        </ul>
+    </div>
 }
