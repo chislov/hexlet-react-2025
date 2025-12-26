af7153d80c39fafb8dfd73920e799263065bbf44
Alexander <chislov@gmail.com>
2025-12-26 12:47:12 +0300

    all 4 test works and all looks good

--- src/CafesTable.jsx
+++ src/CafesTable.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react';
+import FilterCafes from "./FilterCafes";
 
 export default function CafeList() {
   const [cafes, setCafes] = useState([]);
+  const [filteredCafes, setFilteredCafes] = useState([]);
+  const [filter, setFilter] = useState('All'); 
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -11,11 +14,12 @@ export default function CafeList() {
         const response = await fetch('http://localhost:8070/cafes');
         
         if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
+          throw new Error('HTTP error! status: ${response.status}');
         }
         
         const data = await response.json();
-        setCafes(data.cafes); 
+        setCafes(data.cafes);
+        setFilteredCafes(data.cafes);
       } catch (error) {
         setError(error.message);
       } finally {
@@ -26,21 +30,38 @@ export default function CafeList() {
     fetchCafes();
   }, []); 
 
+  // фильтруем
+  useEffect(() => {
+    if (filter === 'All') {
+      setFilteredCafes(cafes);
+    } else {
+      const filtered = cafes.filter(cafe => cafe.subwayCode === filter);
+      setFilteredCafes(filtered);
+    }
+  }, [filter, cafes]);
+
+  const handleFilterChange = (selectedFilter) => {
+    setFilter(selectedFilter);
+  };
+
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
 
-    return <div className='cafesTable'>
-        <ul className="cardsList">
-            {cafes.map(cafe => {
-                return <li className="card">
-                    <img src={cafe.img.length > 0 ? cafe.img : "https://via.placeholder.com/150"} alt={cafe.code} />
-                    <h2>{cafe.name}</h2>
-                    <p>{cafe.decs}</p>
-                    <p>{cafe.address}</p>
-                    <p>{cafe.subwayCode}</p>
-                    <p>{cafe.workTime}</p>
-                </li>
-            })}
-        </ul>
+  return <div id="container" class="container m-3">
+        <div className='cafesTable'>
+            <FilterCafes onFilterChange={handleFilterChange}/>
+            <ul className="cardsList">
+                {filteredCafes.map(cafe => {
+                    return <li className="card" key={cafe.id}>
+                        <img src={cafe.img.length > 0 ? cafe.img : "https://via.placeholder.com/150"} alt={cafe.code} />
+                        <h2>{cafe.name}</h2>
+                        <p>{cafe.decs}</p>
+                        <p>{cafe.address}</p>
+                        <p>{cafe.subwayCode}</p>
+                        <p>{cafe.workTime}</p>
+                    </li>
+                })}
+            </ul>
+        </div>
     </div>
 }
--- src/FilterCafes.jsx
+++ src/FilterCafes.jsx
@@ -1,1 +1,39 @@
+export default function ({onFilterChange}) {
+  const cafeLocations = [
+    {
+        name: "Арбатская",
+        code: "Arbat"
+    },
+    {
+        name: "Александровский сад",
+        code: "Alexanders Garden"
+    },
+    {
+        name: "Московская",
+        code: "Moscow"
+    },
+    {
+        name: "Парк Культуры",
+        code: "Culture"
+    },
+    {
+        name: "Театральная",
+        code: "Theatr"
+    }
+  ];
 
+  const handleChange = (e) => {
+    onFilterChange(e.target.value);
+  }
+    
+    return (
+        <div class="controls">
+            <select name="subway" id="subway" onChange={handleChange}>
+                <option value="All" selected>Все</option>
+                {cafeLocations.map((location) => (
+                    <option key={location.code} value={location.code}>{location.name}</option>
+                ))}
+            </select>
+        </div>
+    )
+}
